74de8de23b12d191ab5c05d931261f0441620bb8
toimurhasan <2imur.hasan@gmail.com>
2025-09-30 19:14:15 +0600

    navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 
 export const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Events</a>
+      </li>
+      <li>
+        <a>My Bookings</a>
+      </li>
+      <li>
+        <a>Add Event</a>
+      </li>
+      <li>
+        <a>Profile</a>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
@@ -26,49 +45,13 @@ export const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Eventify</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
         <a className="btn">Button</a>
